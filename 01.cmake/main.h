e9fea15c17778abcd487df4c34289c866b26bd7b
zeesooho <wltngh00@gmail.com>
2023-01-31 21:58:03 +0900

    Makefile syntax prac

---
+++ 01.cmake/main.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void printLine();
+
+void printLines(int length);
---
+++ 01.cmake/printLines.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+void printLines(int length){
+    for(int i=0; i<length; i++)
+        printLine();
+}
+    
