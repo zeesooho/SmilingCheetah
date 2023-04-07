b5a221bf676a920e9c1e045a4a4bc7c2d024538b
zeesooho <wltngh00@gmail.com>
2023-04-07 10:23:57 +0900

    add data strucre folder

---
+++ 03.data_structure/SinglyLinkedList/SinglyLinkedList.c
@@ -0,0 +1,68 @@
+#include "SinglyLinkedList.h"
+
+static ListNode* create_node(element e){
+    ListNode* node = (ListNode*)malloc(sizeof(ListNode));
+    node->e = e;
+    node->link = NULL;
+    return node;
+}
+
+SinglyLinkedList* create_singly_linked_list(){
+    SinglyLinkedList* list = (SinglyLinkedList*)malloc(sizeof(SinglyLinkedList));
+    list->head = NULL;
+    list->size = 0;
+    return list;
+}
+
+int insert(SinglyLinkedList* list, element e, int pos){
+    if(e < 0) return 0;
+    if(pos > list->size || pos < 0) return 0;
+    if(pos == 0){
+        ListNode* newNode = create_node(e);
+        newNode->link = list->head;
+        list->head = newNode;
+        list->size += 1;
+        return 1;
+    }
+    ListNode* curr = list->head;
+    ListNode* prev = NULL;
+    for(int i=0; i<pos; i++){
+        prev = curr;
+        curr = curr->link;
+    }
+    ListNode* newNode = create_node(e);
+    prev->link = newNode;
+    newNode->link = curr;
+    list->size += 1;
+    return 1;
+}
+
+element get(SinglyLinkedList* list, int pos){
+    if(pos >= list->size) return -1;
+    ListNode* curr = list->head;
+    for(int i=0; i<pos; i++){
+        curr = curr->link;
+    }
+    return curr->e;
+}
+
+int remove_at(SinglyLinkedList* list, int pos){
+    if(pos >= list->size || pos < 0) return 0;
+    if(pos == 0){
+        ListNode* temp = list->head;
+        list->head = temp->link;
+        free(temp);
+        list->size -= 1;
+        return 1;
+    }
+    ListNode* curr = list->head;
+    ListNode* prev = NULL;
+    for(int i=0; i<pos; i++){
+        prev = curr;
+        curr = curr->link;
+    }
+    prev->link = curr->link;
+    free(curr);
+    list->size -= 1;
+    return 1;
+}
---
+++ 03.data_structure/SinglyLinkedList/SinglyLinkedList.h
@@ -0,0 +1,36 @@
+#ifndef __SINGLY_LINKED_LIST_H__
+#define __SINGLY_LINKED_LIST_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef int element;
+
+typedef struct ListNode{
+    element e;
+    struct ListNode* link;
+}ListNode;
+
+static ListNode* create_node(element e);
+
+typedef struct SinglyLinkedList{
+    int size;
+    struct ListNode* head;
+}SinglyLinkedList;
+
+SinglyLinkedList* create_singly_linked_list();
+
+int insert_front(SinglyLinkedList* list, element e);
+int insert_back(SinglyLinkedList* list, element e);
+int insert(SinglyLinkedList* list, element e, int pos);
+
+element get(SinglyLinkedList* list, int pos);
+element pop_front(SinglyLinkedList* list);
+element pop_back(SinglyLinkedList* list);
+
+// int remove(SinglyLinkedList* list, element e);
+int remove_at(SinglyLinkedList* list, int pos);
+
+int index_of(SinglyLinkedList* list, element e);
+
+#endif
