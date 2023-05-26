9c4b34f0a09052423d3c21dd4e5d3a76512b616c
zeesooho <wltngh00@gmail.com>
2023-05-26 18:38:08 +0900

    save 0524

---
+++ 03.data_structure/MinSpanningTree/MST.h
@@ -0,0 +1,15 @@
+#ifndef __MINIMUM_SPANNING_TREE_H__
+#define __MINIMUM_SPANNING_TREE_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Student{
+    int name;
+    int distName;
+    int weight;
+}Student;
+
+void heapify();
+
+#endif
---
+++ 03.data_structure/MinSpanningTree/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "MST.h"
+
+int main(int argc, char **argv){
+    FILE *fp = fopen("students2.txt", "r");
+    if (fp == NULL)
+        exit(1);
+
+    char line[1024];
+    fgets(line, sizeof(line), fp);
+    while (fgets(line, sizeof(line), fp) != NULL){
+        int src = atoi(strtok(line, "\t"));
+        int dist = atoi(strtok(NULL, "\t"));
+        int weight = atoi(strtok(NULL, "\t"));
+        // TODO insert students node
+        printf("src = %d, dist = %d, weight = %d\n", src, dist, weight);
+    }
+    fclose(fp);
+
+
+    return 0;
+}
